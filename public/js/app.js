c3821d07164888371265f8485451fe37810de25e
Lucas Mazzoni <hellolucasm@gmail.com>
2022-11-10 13:40:51 +0100

    Adding style to index page view

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,31 @@
-console.log("JS browser side working!")
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+const messageOne = document.querySelector('#msgOne');
+const messageTwo= document.querySelector('#msgTwo');
+const messageThree = document.querySelector('#msgThree');
+
+weatherForm.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    const location = search.value
+    messageOne.textContent = "Loading..."
+    
+    fetch(`/weather?location=${location}`).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error){
+                console.log(data.error)
+                messageOne.textContent = JSON.stringify(data.error);
+                messageTwo.textContent = ""; 
+                messageThree.textContent = ""; 
+            }else{
+                console.log(data)
+                messageOne.textContent = "You Wrote" + JSON.stringify(data.locationProvided);
+                messageTwo.textContent = "Location founded "  + JSON.stringify(data.locationFound);  
+                messageThree.textContent = `Temperature: ${JSON.stringify(data.weather.current.temperature)}, 
+                                            Time: ${JSON.stringify(data.weather.location.localtime)}` 
+            }; 
+        })
+    })
+
+
+});
